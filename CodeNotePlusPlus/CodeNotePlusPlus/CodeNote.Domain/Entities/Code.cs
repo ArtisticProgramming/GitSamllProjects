479eb84db181dcf7760b0ebdabd0055114019886
Mostafa <36862442+ArtisticProgramming@users.noreply.github.com>
2021-08-28 20:41:08 +0200

    Changes in codeNotePP

--- CodeNotePlusPlus/CodeNotePlusPlus/CodeNote.Domain/Entities/Code.cs
+++ CodeNotePlusPlus/CodeNotePlusPlus/CodeNote.Domain/Entities/Code.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using CodeNote.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CodeNote.Domain.Entities
 {
-    public class Code
+    public class Code : EntityBase
     {
+        public long Description { get; set; }
+        public string CodeSnippet { get; set; }
+        public string ProgrammingLanguages { get; set; }
+        public NoteCode NoteCode { get; set; }
     }
 }
